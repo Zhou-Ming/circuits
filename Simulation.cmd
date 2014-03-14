6a20be51e89bebecf67423c9e52191d5322f692e
Zhou Ming <mzhou.ismer@gmail.com>
2014-03-15 02:47:47 +1030

    added transmission line model.

--- Simulation.cmd
+++ Simulation.cmd
@@ -1,5 +1,8 @@
 .control
 OP
 AC LIN 200 1GigHz 5GigHz
-plot db(v(Vout)/v(Vin)) vp(Vout)
+plot db(v(Vout)/v(Vin))
+plot vp(Vout)
 .endc
+.model emitter_lline ltra rel=1 r=12.45 g=0 l=8.972e-9 c=0.468e-12
++len=16 steplimit compactrel=1.0e-3 compactabs=1.0e-14
---
+++ simulation/models/tline/emitter_tline.sp
@@ -0,0 +1,1 @@
+.model emitter_tline ltra r=40.35 g=0 l=3.874e-07 c=8.858e-11 len=2.5e-04
