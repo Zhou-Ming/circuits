d3230a403ee74bf2ec0af749aa96b887f55b406a
Zhou Ming <mzhou.ismer@gmail.com>
2014-03-15 12:26:11 +1030

    Removed model from Simulation.cmd

--- Simulation.cmd
+++ Simulation.cmd
@@ -4,5 +4,3 @@ AC LIN 200 1GigHz 5GigHz
 plot db(v(Vout)/v(Vin))
 plot vp(Vout)
 .endc
-.model emitter_lline ltra rel=1 r=12.45 g=0 l=8.972e-9 c=0.468e-12
-+len=16 steplimit compactrel=1.0e-3 compactabs=1.0e-14
